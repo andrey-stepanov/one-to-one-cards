1323528a28f8506c5aaf4e5d6bb621067c42d30b
Andrey Stepanov <bomberman3k@gmail.com>
2023-01-02 16:42:41 +0000

    Added logic for questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
 import "normalize.css";
 import "./styles.css";
+import questions from "./questions.json";
+import React, { useState } from 'react';
+
+function getQuestion(){
+  let index = Math.floor(Math.random() * questions.length);
+  return questions[index];
+}
 
 export default function App() {
+  const question = getQuestion();
+  const [card,setCard] = useState({isOpen: false, title: question.category, text: question.question});
+  
+  function openCard(){
+    setCard({...card, isOpen: true});
+  }
+  
+  function nextCard(){
+    const question = getQuestion();
+    setCard({isOpen: false, title: question.category, text: question.question});
+  }
+  
+  let cardClassName = "card " + (card.isOpen ? "card_open" : "" );
+  
+
   return (
     <div className="app">
-      <h1>
-        Tap on the card to reveal the question for an insightful 1-to-1 meeting
-      </h1>
-      <div className="card ">
-        <div className="card__title">About Manager</div>
+      <div className="top">
+      {
+        !card.isOpen && 
+        <h1>
+          Tap on the card to reveal the question for an insightful 1-to-1 meeting
+        </h1>
+      }
+      { 
+        card.isOpen && 
+        <button className="btn" onClick={nextCard}>Next card</button>
+      }
+      </div>
+
+      <div className={cardClassName} onClick={openCard}>
+        <div className="card__title">{card.title}</div>
         <div>
-          At what point in the past week were you most frustrated with or
-          discouraged by your work? What can I do to help you manage that? ❧
+          {card.text} ❧
         </div>
       </div>
-      <button className="btn">Take 5 cards</button>
-      <button className="btn">Select topics</button>
+      {/* <button className="btn">Take 5 cards</button>
+      <button className="btn">Select topics</button> */}
       <div className="footer">
         Made with ️
         <span role="img" aria-label="love">
