4d6dabc4f9932270136d075bfe856ff27c6d6117
Andrey Stepanov <bomberman3k@gmail.com>
2023-01-04 06:26:00 +0000

    Amended animation timing

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,67 @@
 import "normalize.css";
 import "./styles.css";
 import questions from "./questions.json";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function getQuestion(){
+function getQuestion() {
   let index = Math.floor(Math.random() * questions.length);
   return questions[index];
 }
 
 export default function App() {
   const question = getQuestion();
-  const [card,setCard] = useState({isOpen: false, isMovingOut: false, title: question.category, text: question.question});
-  
-  function openCard(){
-    setCard({...card, isOpen: true});
+  const [card, setCard] = useState({
+    isOpen: false,
+    isMovingOut: false,
+    title: question.category,
+    text: question.question,
+  });
+
+  function openCard() {
+    setCard({ ...card, isOpen: true });
   }
-  
-  function nextCard(){
-    setCard({...card, isOpen: false, isMovingOut: true});
-    // delay until flip animation finishes
+
+  function nextCard() {
+    setCard({ ...card, isOpen: false, isMovingOut: true });
+    // delay until flip & slide animation finishes
     setTimeout(() => {
-        const question = getQuestion();
-        setCard({isOpen: false, isMovingOut: false, title: question.category, text: question.question});
-    }, 300); 
+      const question = getQuestion();
+      setCard({
+        isOpen: false,
+        isMovingOut: false,
+        title: question.category,
+        text: question.question,
+      });
+    }, 300 + 100);
   }
-  
+
   return (
     <div className="app">
       <div className="top">
-      {
-        !card.isOpen ?
-        <h1>
-          Tap on the card to reveal the question for an insightful 1-to-1 meeting
-        </h1>
-        : 
-        <button className="btn" onClick={nextCard}>Next card</button>
-      }
+        {!card.isOpen ? (
+          <h1>
+            Tap on the card to reveal the question for an insightful 1-to-1
+            meeting
+          </h1>
+        ) : (
+          <button className="btn" onClick={nextCard}>
+            Next card
+          </button>
+        )}
       </div>
-      <div className={'card ' + (card.isOpen ? 'card_open' : '') + ' ' +  (card.isMovingOut ? 'card_movingout' : '')}>
+      <div
+        className={
+          "card " +
+          (card.isOpen ? "card_open" : "") +
+          " " +
+          (card.isMovingOut ? "card_movingout" : "")
+        }
+      >
         <div className="card__flipper">
-          <div className="card__front" onClick={openCard}>
-          </div>
+          <div className="card__front" onClick={openCard}></div>
           <div className="card__back">
             <div className="card__title">{card.title}</div>
-            <div>
-              {card.text} ❧
-            </div>
+            <div>{card.text} ❧</div>
           </div>
         </div>
       </div>
